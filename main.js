d10f10b6005e6e403f45b163d0805f7984c47c6e
p47n <patelmuhammed7777@gmail.com>
2025-03-24 23:23:35 -0230

    Fixed bug for displaying prayer times

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const axios = require("axios");
 
 let mainWindow;
@@ -8,31 +8,35 @@ app.whenReady().then(() => {
     width: 600,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: true,  // Enable nodeIntegration to allow `require` in renderer
+      contextIsolation: false, // Disable contextIsolation for ease of use during dev (make sure to enable it before production)
     },
   });
 
   mainWindow.loadFile("index.html");
 
-  getPrayerTimes(); // Fetch prayer times when app starts
+  // Listen for renderer request to get prayer times
+  ipcMain.on("get-prayer-times", (event) => {
+    getPrayerTimes().then((times) => {
+      event.reply("prayer-times", times);  // Send prayer times to renderer
+    });
+  });
 });
 
 async function getPrayerTimes() {
   try {
-    const response = await axios.get(
-      "https://api.aladhan.com/v1/timingsByCity",
-      {
-        params: {
-          city: "Toronto", // You can later make this user-configurable
-          country: "Canada",
-          method: 2, // Calculation method
-        },
-      }
-    );
+    const response = await axios.get("https://api.aladhan.com/v1/timingsByCity", {
+      params: {
+        city: "Toronto", // You can later make this user-configurable
+        country: "Canada",
+        method: 2, // Calculation method
+      },
+    });
 
     const timings = response.data.data.timings;
-    console.log("Prayer Times:", timings);
+    return timings;  // Return timings to send to renderer
   } catch (error) {
     console.error("Error fetching prayer times:", error);
+    return {}; // Return empty object if there's an error
   }
 }
